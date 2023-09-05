cf09357a16042441d23343fff1d8822b514f4212
Sara <saracoppola996@gmail.com>
2023-09-05 15:14:40 +0200

    third commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
 
-    public static void checkScore(double score){
+    public static void checkScore(double score) {
         if (0 < score && score <= 50.00) {
-                System.out.println("Average score");
-            }else if (50.00 < score && score <= 100.00) {
-                System.out.println("Very good score");
-            }else{
+            System.out.println("Average score");
+        } else if (50.00 < score && score <= 100.00) {
+            System.out.println("Very good score");
+        } else {
             throw new ArithmeticException("Score is out of scale!");
         }
     }
